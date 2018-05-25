3d2d93e2872d8f4cd8cba597cb8a0a74da7c5398
sonali2896 <sonalipawar17.sp@gmail.com>
2018-05-25 12:47:12 +0000

    pagination is added to all listing api

--- app/Http/Controllers/Seller/GroupController.php
+++ app/Http/Controllers/Seller/GroupController.php
@@ -16,6 +16,7 @@ use App\User;
 use App\Group;
 use App\Seller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Lumen\Routing\Controller as BaseController;
@@ -36,18 +37,30 @@ class GroupController extends BaseController
     public function getGroupList(){
         try {
             $user = Auth::user();
+            $currentPage = Input::get('page', 1)-1;
             $seller_id= Seller::where('user_id',$user['id'])->pluck('id')->first();
             $seller_groups = Group::where('seller_id',$seller_id)->get();
             $iterator = 0;
             $groupList = array();
+            $group_array = collect($seller_groups)->toArray();
+            $pagedData = array_slice($group_array, $currentPage * $this->perPage, $this->perPage);
+
             foreach ($seller_groups as $key => $group) {
                 $groupList[$iterator]['group_id'] = $group['id'];
                 $groupList[$iterator]['group_name'] = $group['name'];
                 $groupList[$iterator]['total_member'] = $group->groupCustomer->count();
-
                 $iterator++;
             }
-            $data['select_groups'] = $groupList;
+            $data = [
+                'select_group' => $pagedData,
+                'pagination' => [
+                    'page' => $currentPage + 1 ,
+                    'perPage' => $this->perPage,
+                    'pageCount' => count($pagedData),
+                    'totalCount' => count($groupList),
+                ],
+            ];
+
             $message = 'Success';
             $status = 200;
         } catch (\Exception $e) {
@@ -151,8 +164,10 @@ class GroupController extends BaseController
             $iterator = 0;
             $offerList = array();
 
+
             foreach ($group_offers_id as $key => $group_offer_id) {
                 $offers = Offer::where('id', $group_offer_id)->get();
+
 
                 foreach ($offers as $key2 => $offer) {
                     $offerList[$iterator]['offer_id'] = $offer['id'];
@@ -168,7 +183,19 @@ class GroupController extends BaseController
                     $iterator++;
                 }
             }
-            $data['group_offers'] = $offerList;
+            $currentPage = Input::get('page', 1)-1;
+            $group_offer_array = collect($offerList)->toArray();
+            $pagedData = array_slice($group_offer_array, $currentPage * $this->perPage, $this->perPage);
+
+            $data = [
+                'group_offers' => $pagedData,
+                'pagination' => [
+                    'page' => $currentPage + 1 ,
+                    'perPage' => $this->perPage,
+                    'pageCount' => count($pagedData),
+                    'totalCount' => count($offerList),
+                ],
+            ];
             $message = 'Success';
             $status = 200;
         } catch (\Exception $e) {
